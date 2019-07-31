ddce6f69d605bda64048fcd409395761e4bdbe77
Aditya Tanikanti <aditya.tanikanti@gmail.com>
2019-07-31 15:00:27 -0500

    Added workflow

--- gencwl.py
+++ gencwl.py
@@ -17,46 +17,54 @@ import cwlgen
 if __name__ == "__main__":
 
     # Create a tool
-    cwl_tool = cwlgen.CommandLineTool(tool_id='west_tool',
-                                      label='generates pw.out and wstat.out files',
-                                      base_command='sh')
+    # Write in an output file
+    #cwl_tool.export()
+    #cwl_tool.export("west.cwl")
+    
+    cwl_tool = cwlgen.workflow.Workflow(workflow_id='west_tool',
+                                        label='generates pw.out and wstat.out files',
+                                        doc='Tool to generate west code',
+                                        cwl_version='v1.0')
+    
+    
 
     # Add  inputs (script_file, URLs, type, no_of_cores)
     script_binding = cwlgen.CommandLineBinding(position=1)
-    script_file = cwlgen.CommandInputParameter('script_file',
+    script_file = cwlgen.workflow.InputParameter('script_file',
                                               param_type='File',
                                               input_binding=script_binding,
                                               doc='West script which generates pw.out, wstat.out based on inputs')
     cwl_tool.inputs.append(script_file)
-    
+
     file_binding = cwlgen.CommandLineBinding(position=2)
-    input_file = cwlgen.CommandInputParameter('input_file',
+    input_file = cwlgen.workflow.InputParameter('input_file',
                                               param_type='File',
                                               input_binding=file_binding,
                                               doc='Input file for west code. Can be pw.in, wstat.in, wfreq.in')
     cwl_tool.inputs.append(input_file)
-    
+
 
     url_binding = cwlgen.CommandLineBinding(position=3)
-    urls = cwlgen.CommandInputParameter('URLs',
+    urls = cwlgen.workflow.InputParameter('URLs',
                                            param_type='string',
                                            input_binding=url_binding,
                                            doc='Urls to download upf files used for west scripts')
     cwl_tool.inputs.append(urls)
-    
+
     script_type_binding = cwlgen.CommandLineBinding(position=4)
-    type_script = cwlgen.CommandInputParameter('script_type',
+    type_script = cwlgen.workflow.InputParameter('script_type',
                                            param_type='string',
                                            input_binding=script_type_binding,
                                            doc='choose between pw, wstat, wfreq')
     cwl_tool.inputs.append(type_script)
 
     core_binding = cwlgen.CommandLineBinding(position=5)
-    cores = cwlgen.CommandInputParameter('no_of_cores',
+    cores = cwlgen.workflow.InputParameter('no_of_cores',
                                            param_type='int',
                                            input_binding=core_binding,
                                            doc='choose between pw, wstat, wfreq')
     cwl_tool.inputs.append(cores)
+
     
     # Add 1 output
     output_bind = cwlgen.CommandOutputBinding(glob="*.out")
@@ -66,15 +74,12 @@ if __name__ == "__main__":
                                            doc='Output File generated with west code')
     cwl_tool.outputs.append(output)
 
-    # Add documentation
-    cwl_tool.doc = "Runs west code specifically pw, wstat or wfreq"
 
-    # Add Metadata
-    metadata = {'name': 'West',
-                'about': 'West code runs pw , wstat for an input file.'}
-    cwl_tool.metadata = cwlgen.Metadata(**metadata)
-    cwl_tool.metadata = cwlgen.Metadata(**metadata)
-
-    # Write in an output file
-    cwl_tool.export()
-    cwl_tool.export("west.cwl")
+    workflow_pw = cwlgen.workflow.WorkflowStep('pw_tool',
+                                                'west.cwl')
+    #workflow_pw_in = workflow_pw.WorkflowStepInput('script_file','script_file') 
+    workflow_pw.inputs.append(script_file)
+    #workflow_pw.outputs.append(output)	
+    cwl_tool.steps.append(workflow_pw)
+    #cwl_tool.outputs.append(output)
+    cwl_tool.export("w.cwl")
--- west.cwl
+++ west.cwl
@@ -5,40 +5,41 @@ $namespaces:
 baseCommand: sh
 class: CommandLineTool
 cwlVersion: v1.0
-doc: Runs west code specifically pw, wstat or wfreq
+doc: Runs west code for different input paramaters
 id: west_tool
 inputs:
-- doc: West script which generates pw.out, wstat.out based on inputs
+- doc: West shell script which generates output based on inputs using docker
   id: script_file
   inputBinding:
     position: 1
   type: File
-- doc: Input file for west code. Can be pw.in, wstat.in, wfreq.in
+- doc: Input file for west code. Can be pw.in, wstat.in
   id: input_file
   inputBinding:
     position: 2
   type: File
-- doc: Urls to download upf files used for west scripts
+- doc: Urls to download upf files used for pw, wstat scripts
   id: URLs
   inputBinding:
     position: 3
   type: string
-- doc: choose between pw, wstat, wfreq
+- doc: choose pw or wstat
   id: script_type
   inputBinding:
     position: 4
   type: string
-- doc: choose between pw, wstat, wfreq
+- doc: choose 2, 3, 4
   id: no_of_cores
   inputBinding:
     position: 5
   type: int
-label: generates pw.out and wstat.out files
+label: calculates pw.x, wstat.x and generates pw.out,wstat.out file
 outputs:
-- doc: Output File generated with west code
-  id: output
+- doc: Output File generated with west pw.x or wstat.x code
+  id: west_output_file
   outputBinding:
     glob: '*.out'
   type: File
-s:about: West code runs pw , wstat for an input file.
+s:about: West code runs pw for an input pw.in file to generate pw.out, or wstat for
+  wstat.in with wstat.out file
 s:name: West
